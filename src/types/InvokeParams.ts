e74054952322c107ac9a151c94010b48c7a2cc52
YanagiEiichi <576398868@qq.com>
2023-05-06 17:05:55 +0800

    Update comments

--- src/types/InvokeParams.ts
+++ src/types/InvokeParams.ts
@@ -1,20 +1,41 @@
 export interface InvokeParams {
   /**
-   * NOTE: Some MiniProgram platforms can only support GET or POST methods, so using a RESTful API is not the best solution for MiniPrograms.
+   * An HTTP method, such as GET, POST.
+   * NOTE: Some MiniProgram platforms can only support GET or POST methods,
+   *       so using a RESTful API is not the best solution for MiniPrograms.
    */
   method: string;
 
+  /**
+   * Specify a request URL.
+   */
   url: string;
 
+  /**
+   * Specify a request data.
+   * NOTE: For GET method, the `data` will be converted to a query string.
+   */
   data?: Record<string, unknown>;
 
+  /**
+   * Specify a timeout in milliseconds.
+   */
   timeout?: number;
 
+  /**
+   * Specify the reqeust headers.
+   * @example { Accept: 'application/json' }
+   */
   headers?: Record<string, string>;
 
   /**
-   * NOTE: In browsers, the file is represented as a Blob or File object, whereas in other MiniProgram platforms, the file is represented as a string filePath.
-   * NOTE: MiniProgram platforms doese not suport multiple files uploading in once.
+   * Specify the files that your want to upload.
+   * This parameter indecates the HTTP method POST, and Content-Type multipart/form-data.
+   * If you provided a conflict configuration, an error will be thrown.
+   * NOTE: In browsers, the file is represented as a Blob or File object, whereas in other MiniProgram platforms,
+   *       the file is represented as a string filePath.
+   * NOTE: The MiniProgram platforms does not support uploading multiple files at once,
+   *       if you specify more than one file, a BatchUploadError will be thrown.
    */
   files?: Record<string, Blob | File | string>;
 }
--- src/types/InvokeResult.ts
+++ src/types/InvokeResult.ts
@@ -1,5 +1,16 @@
 export interface InvokeResult<T = unknown> {
+  /**
+   * Response status code.
+   */
   statusCode: number;
+
+  /**
+   * Parsed response body.
+   */
   data: T;
+
+  /**
+   * Response headers.
+   */
   headers: Record<string, string>;
 }
