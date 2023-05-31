d1d21262041ebab99eddcad83e8c2d11c28106ad
YanagiEiichi <576398868@qq.com>
2023-05-31 16:06:47 +0800

    Refactor fixInvokeParams.ts

--- src/utils/fixInvokeParams.ts
+++ src/utils/fixInvokeParams.ts
@@ -9,21 +9,51 @@ export const fixInvokeParams = ({ ...args }: InvokeParams) => {
   args.method = method;
 
   // For GET or HEAD methods, mixin the data into the QS.
+  // NOTE: Some MiniProgram platforms only support this feature in the GET method.
+  //       Screw it, we will implement it in a better way.
+  //
   if (method === 'GET' || method === 'HEAD') {
     const { url, data } = args;
 
+    // Matcha a URL
+    // Group 1: Origin & Path
+    // Group 2: Query String
+    // Group 3: Fragment (unused)
     const matches = url.match(/^(.*?)(\?.*?)?(#.*)?$/);
-    // This pattern can match any string and will never be null.
+
+    // The above pattern can match any string and will never be null.
     assertNotNull(matches);
 
-    const solid = matches[1]; // scheme + path
-    let query = matches[2] || ''; // query string
-    // matches[3] is a fragment, drop it.
+    const originAndPath = matches[1];
+    const preservedQs = matches[2] || '';
 
-    const aqs = buildQs(data);
-    if (aqs) query += (query ? '&' : '?') + aqs;
+    // Build qs from object.
+    // NOTE: The behavior of qs buider here is different from some MiniProgram platforms.
+    //       Some MiniProgram platforms simply convert values to JSON strings unless it was originally a string.
+    //       Here, we consider more complex cases of
+    //       1. For an array value, separate it as a form of multiple value in single key.
+    //       2. Undefined values will be ignored instead of being converted to "undefined" strings.
+    //       3. Support specially for `BigInt` type, which cannot be serialized to a JSON by default.
+    //       4. Support specially for `Date` type, which may be serialized to a string wrapped by a quote by default.
+    //
+    const appendingQs = buildQs(data);
 
-    args.url = solid + query;
+    // Merge preservedQs and appendingQs.
+    // NOTE: Some MiniProgram platforms cannot support multiple values in same key.
+    //       For examples:
+    //       - url: "/path?a=1” data: { a: 2 }, which will be merged to "/path?a=2".
+    //       - url: "/path?a=1&a=2” data: {}, which will be merged to "path?a=2".
+    //       Obviously, this is NOT a smart implementation.
+    //       Although there is no definitive standard for the multiple values in single key,
+    //       but most web frameworks such as Java Spring Framework define this usage to transfer an array of arguments.
+    //       Therefore, our implementation retains support for the multiple values in single key.
+    //
+    let qs = preservedQs;
+    if (appendingQs) {
+      qs += (qs ? '&' : '?') + appendingQs;
+    }
+
+    args.url = originAndPath + qs;
     args.data = undefined;
   }
 
--- src/utils/toDataString.ts
+++ src/utils/toDataString.ts
@@ -2,10 +2,19 @@ export const toDataString = (s: unknown) => {
   switch (typeof s) {
     case 'string':
       return s;
+
+    // Convert to string directly, because a `BigInt` type cannot be serialized to a JSON.
     case 'bigint':
       return String(s);
+
     default:
+      // If simply convert the `Date` to string using `JSON.stringify`, the result will be wrapped by double quotes.
+      // Because stringify will call the toJSON method first, and stringify the string result as a JSON string,
+      // as we as known, a JSON string is wrapped by double quotes.
+      // Most web frameworks cannot support that weird serialization for `Date` type.
+      // Therefore, using the result of toJSON directly is the better solution.
       if (s instanceof Date) return s.toJSON();
+
       return JSON.stringify(s);
   }
 };
