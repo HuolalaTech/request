ba9ca6e7239fdc8d7dcbfedfb974d5c5afed7598
YanagiEiichi <576398868@qq.com>
2024-07-04 10:38:44 +0800

    Update the return type of isContentType

--- src/utils/predicates.ts
+++ src/utils/predicates.ts
@@ -1,5 +1,7 @@
 import { CONTENT_TYPE, APPLICATION_JSON, MULTIPART_FORM_DATA, WWW_FORM_URLENCODED } from '../constants';
 
+type MultiCases<T extends string> = T | Lowercase<T> | Uppercase<T>;
+
 const createMediaTypePredicate = <T extends string>(ct: T) => {
   /**
    * @see https://datatracker.ietf.org/doc/html/rfc7231#section-3.1.1.1
@@ -10,12 +12,11 @@ const createMediaTypePredicate = <T extends string>(ct: T) => {
    *
    * NOTE: The type, subtype, and parameter name tokens are case-insensitive.
    */
-  type CS<T extends string> = `${Lowercase<T> | Uppercase<T>}`;
-  type PS<T extends string> = `${T}; ${string}`;
+  type AndParameter<T extends string> = `${T}; ${string}`;
   const pattern = new RegExp(`^${ct}(?:\\s*;|$)`, 'i');
   // Infact, this pattern only tests the input with the "i" flag, and does not completely match the type declaration.
   // Nevertheless, a stricter type constraint is preferable to the default "string" type.
-  return RegExp.prototype.test.bind(pattern) as (s: string) => s is CS<T> | PS<CS<T>>;
+  return RegExp.prototype.test.bind(pattern) as (s: string) => s is MultiCases<T> | MultiCases<AndParameter<T>>;
 };
 
 /**
@@ -36,7 +37,9 @@ export const isApplicationJson = createMediaTypePredicate(APPLICATION_JSON);
 /**
  * Detects a Content-Type string case-insesitive
  */
-export const isContentType = RegExp.prototype.test.bind(new RegExp(`^${CONTENT_TYPE}$`, 'i'));
+export const isContentType = RegExp.prototype.test.bind(new RegExp(`^${CONTENT_TYPE}$`, 'i')) as (
+  s: string,
+) => s is MultiCases<typeof CONTENT_TYPE>;
 
 export const isRecord = (u: unknown): u is Record<PropertyKey, unknown> => typeof u === 'object' && u !== null;
 
